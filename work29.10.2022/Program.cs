dd635389e0ea9d294b71ce810d8a6975ebdc5e1b
informant <artemiy.solodkin@mail.ru>
2022-10-29 17:27:39 +0500

    Start Work V3

---
+++ work29.10.2022/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace work29._10._2022
+{
+    using System;
+
+    namespace MyGitflowTask
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+            }
+        }
+    }
+}
